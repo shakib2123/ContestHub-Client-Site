b989da1fa68fae88a228fe723b04b7977ebfe433
shakib2123 <mshakibalhasan21@gmail.com>
2023-11-25 16:00:09 +0600

    tab filter method implimented

--- src/Pages/AllContest/AllContest.jsx
+++ src/Pages/AllContest/AllContest.jsx
@@ -1,25 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxios from "../../hooks/useAxios";
 import Contest from "./Contest";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { FaBriefcase, FaFirstAid, FaGamepad } from "react-icons/fa";
+import { FaFilePen } from "react-icons/fa6";
 import "react-tabs/style/react-tabs.css";
 import { useState } from "react";
 
 const AllContest = () => {
   const axiosSecure = useAxios();
-  const [type, setType] = useState("");
-  const categories = ["salad", "soup", "dessert", "pizza", "drinks"];
-  const initialIndex = categories.indexOf(0);
-  const [tab, setTab] = useState(initialIndex);
-  console.log(tab);
+  const [category, setCategory] = useState("");
+  const handleClick = (type) => {
+    setCategory(type);
+  };
+  console.log(category);
   const {
     data: allContest = [],
     isLoading,
     isError,
   } = useQuery({
-    queryKey: ["allContest"],
+    queryKey: ["allContest", category],
     queryFn: async () => {
-      const res = await axiosSecure.get("/contests");
+      const res = await axiosSecure.get(`/contests?category=${category}`);
       return res.data;
     },
   });
@@ -27,21 +28,53 @@ const AllContest = () => {
 
   return (
     <div className="max-w-7xl mx-auto">
-      <Tabs defaultIndex={tab} onSelect={(index) => setTab(index)}>
-        <TabList>
-          <Tab>SALAD</Tab>
-          <Tab>SOUP</Tab>
-          <Tab>DESSERTS</Tab>
-          <Tab>PIZZA</Tab>
-          <Tab>DRINKS</Tab>
-        </TabList>
+      <div className="flex gap-4 items-center overflow-auto px-4">
+        <div
+          onClick={() => handleClick("Business Contest")}
+          className={`flex flex-col  items-center justify-center gap-2 p-3 hover:text-neutral-800 transition cursor-pointer ${
+            category === "Business Contest"
+              ? " text-blue-600 border-blue-600 border-b-4"
+              : ""
+          }`}
+        >
+          <FaBriefcase size={26}></FaBriefcase>
+          <p className="text-sm font-medium">Business Contest</p>
+        </div>
+        <div
+          onClick={() => handleClick("Medical Contest")}
+          className={`flex flex-col  items-center justify-center gap-2 p-3 hover:text-neutral-800 transition cursor-pointer ${
+            category === "Medical Contest"
+              ? " text-blue-600 border-blue-600 border-b-4"
+              : ""
+          }`}
+        >
+          <FaFirstAid size={26} />
+          <p className="text-sm font-medium">Medical Contest</p>
+        </div>
+        <div
+          onClick={() => handleClick("Article Writing")}
+          className={`flex flex-col  items-center justify-center gap-2 p-3 hover:text-neutral-800 transition cursor-pointer ${
+            category === "Article Writing"
+              ? " text-blue-600 border-blue-600 border-b-4"
+              : ""
+          }`}
+        >
+          <FaFilePen size={26} />
+          <p className="text-sm font-medium">Article Writing</p>
+        </div>
+        <div
+          onClick={() => handleClick("Gaming")}
+          className={`flex flex-col  items-center justify-center gap-2 p-3 hover:text-neutral-800 transition cursor-pointer ${
+            category === "Gaming"
+              ? " text-blue-600 border-blue-600 border-b-4"
+              : ""
+          }`}
+        >
+          <FaGamepad size={26} />
+          <p className="text-sm font-medium">Gaming</p>
+        </div>
+      </div>
 
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-      </Tabs>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {allContest.map((contest) => (
           <Contest key={contest._id} contest={contest}></Contest>
--- src/Pages/AllContest/Contest.jsx
+++ src/Pages/AllContest/Contest.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 const Contest = ({ contest }) => {
   const { contestName, image, attendance, description } = contest;
   const shortDesc = description.slice(0, 100);
-  console.log(shortDesc);
+
 
   return (
     <>
@@ -17,15 +19,17 @@ const Contest = ({ contest }) => {
               <h2 className=" text-lg md:text-2xl">{contestName}</h2>
             </div>
 
-            <p className="text-gray-400 text-lg font-medium">
+            <p className="text-blue-400 text-lg font-medium">
               Attempted count: {attendance}
             </p>
             <div className="m-2 text-sm">
               <p className="text-gray-300">{shortDesc}.....</p>
             </div>
-            <button className="group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-rose-300 hover:before:[box-shadow:_20px_20px_20px_30px_#a21caf] duration-500 before:duration-500 hover:duration-500 underline underline-offset-2 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur hover:underline hover:underline-offset-4  origin-left hover:decoration-2 hover:text-rose-300 relative bg-neutral-800 h-16 w-64 border text-left p-3 text-gray-50 text-base font-bold rounded-lg  overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  after:bg-rose-300 after:right-8 after:top-3 after:rounded-full after:blur-lg">
-              See more
-            </button>
+            <Link to='/contestDetails'>
+              <button className="group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-rose-300 hover:before:[box-shadow:_20px_20px_20px_30px_#a21caf] duration-500 before:duration-500 hover:duration-500 underline underline-offset-2 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur hover:underline hover:underline-offset-4  origin-left hover:decoration-2 hover:text-rose-300 relative bg-neutral-800 h-16 w-64 border text-left p-3 text-gray-50 text-base font-bold rounded-lg  overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  after:bg-rose-300 after:right-8 after:top-3 after:rounded-full after:blur-lg">
+                Details
+              </button>
+            </Link>
           </div>
         </div>
       </div>
